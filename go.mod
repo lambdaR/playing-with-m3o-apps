a24b2b5ef9037da5eb0a867722c3f491b182b106
Daniel Joudat <danieljoudat@gmail.com>
2021-12-15 23:27:25 +0300

    Apps branch main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module djoudat.io/m3oapps/v1
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now().String()
+		greeting := "Apps branch Main => " + t
+		w.Write([]byte(greeting))
+	})
+	http.ListenAndServe(":8080", nil)
+}
