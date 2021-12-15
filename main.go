dccbba0cb9b5b8c0b96f8c776b46abc7b48ee2fd
Daniel Joudat <danieljoudat@gmail.com>
2021-12-16 02:05:34 +0300

    add Updated to greeting

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		t := time.Now().String()
-		greeting := "Apps branch Main => " + t
+		greeting := "Apps branch Main Updated => " + t
 		w.Write([]byte(greeting))
 	})
 	http.ListenAndServe(":8080", nil)
